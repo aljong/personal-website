fa9b37e02df2c843c2ad0690c52b500c7cd5e6fc
aljong <alex.y.jong@gmail.com>
2024-12-19 23:47:52 -0500

    fix page routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aljong.github.io/personal-website/",
+  site: "https://aljong.github.io/",
+  base: '/personal-website',
   integrations: [preact(), tailwind()]
 });
